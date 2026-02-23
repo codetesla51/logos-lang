b928acd2543d08c30f02c44b940a389b3e49588a
Oladele usman <uoladele99@gmail.com>
2026-02-23 12:03:20 +0100

    got basic parser working

--- main.go
+++ main.go
@@ -8,33 +8,39 @@ import (
 )
 
 func main() {
-	prog := &parser.Program{
-		Statements: []parser.Statement{
-			&parser.Identifier{Token: golexer.Token{Type: golexer.IDENT, Literal: "X"}, Value: "X"},
-			&parser.LetStatement{
-				Token: golexer.Token{Type: golexer.LET, Literal: "let"},
-				Name:  &parser.Identifier{Token: golexer.Token{Type: golexer.IDENT, Literal: "y"}, Value: "y"},
-				Value: &parser.InfixExpression{
-					Token:    golexer.Token{Type: golexer.PLUS, Literal: "+"},
-					Operator: "+",
-					Left:     &parser.IntegerLiteral{Token: golexer.Token{Type: golexer.NUMBER, Literal: "5"}, Value: 5},
-					Right:    &parser.IntegerLiteral{Token: golexer.Token{Type: golexer.NUMBER, Literal: "3"}, Value: 3},
-				},
-			},
-			&parser.ReturnStatement{
-				Token:       golexer.Token{Type: golexer.RETURN, Literal: "return"},
-				ReturnValue: &parser.Identifier{Token: golexer.Token{Type: golexer.IDENT, Literal: "y"}, Value: "y"},
-			},
-			&parser.ExpressionStatement{
-				Token: golexer.Token{Type: golexer.IDENT, Literal: "foobar"},
-				Expression: &parser.InfixExpression{
-					Token:    golexer.Token{Type: golexer.PLUS, Literal: "+"},
-					Operator: "+",
-					Left:     &parser.Identifier{Token: golexer.Token{Type: golexer.IDENT, Literal: "foobar"}, Value: "foobar"},
-					Right:    &parser.Identifier{Token: golexer.Token{Type: golexer.IDENT, Literal: "y"}, Value: "y"},
-				},
-			},
-		},
-	}
-	fmt.Println(prog.String())
+	// prog := &parser.Program{
+	// 	Statements: []parser.Statement{
+	// 		&parser.Identifier{Token: golexer.Token{Type: golexer.IDENT, Literal: "X"}, Value: "X"},
+	// 		&parser.LetStatement{
+	// 			Token: golexer.Token{Type: golexer.LET, Literal: "let"},
+	// 			Name:  &parser.Identifier{Token: golexer.Token{Type: golexer.IDENT, Literal: "y"}, Value: "y"},
+	// 			Value: &parser.InfixExpression{
+	// 				Token:    golexer.Token{Type: golexer.PLUS, Literal: "+"},
+	// 				Operator: "+",
+	// 				Left:     &parser.IntegerLiteral{Token: golexer.Token{Type: golexer.NUMBER, Literal: "5"}, Value: 5},
+	// 				Right:    &parser.IntegerLiteral{Token: golexer.Token{Type: golexer.NUMBER, Literal: "3"}, Value: 3},
+	// 			},
+	// 		},
+	// 		&parser.ReturnStatement{
+	// 			Token:       golexer.Token{Type: golexer.RETURN, Literal: "return"},
+	// 			ReturnValue: &parser.Identifier{Token: golexer.Token{Type: golexer.IDENT, Literal: "y"}, Value: "y"},
+	// 		},
+	// 		&parser.ExpressionStatement{
+	// 			Token: golexer.Token{Type: golexer.IDENT, Literal: "foobar"},
+	// 			Expression: &parser.InfixExpression{
+	// 				Token:    golexer.Token{Type: golexer.PLUS, Literal: "+"},
+	// 				Operator: "+",
+	// 				Left:     &parser.Identifier{Token: golexer.Token{Type: golexer.IDENT, Literal: "foobar"}, Value: "foobar"},
+	// 				Right:    &parser.Identifier{Token: golexer.Token{Type: golexer.IDENT, Literal: "y"}, Value: "y"},
+	// 			},
+	// 		},
+	// 	},
+	// }
+	// fmt.Println(prog.String())
+
+	input := `let x = 0.5 + 3; return y; foobar + y;`
+	lexer := golexer.NewLexer(input)
+	parser := parser.NewParser(lexer)
+	program := parser.Parse()
+	fmt.Println(program.String())
 }
--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/codetesla51/golexer/golexer"
@@ -41,6 +42,10 @@ type IntegerLiteral struct {
 	Token golexer.Token
 	Value int64
 }
+type FloatLiteral struct {
+	Token golexer.Token
+	Value float64
+}
 type InfixExpression struct {
 	Token    golexer.Token
 	Left     Expression
@@ -59,6 +64,19 @@ type ReturnStatement struct {
 type ExpressionStatement struct {
 	Token      golexer.Token
 	Expression Expression
+}
+type PrefixParsefn func() Expression
+type InfixParsefn func(Expression) Expression
+
+var precedences = map[golexer.TokenType]int{
+	golexer.EQL:          EQUALS,
+	golexer.NOT_EQL:      EQUALS,
+	golexer.LESS_THAN:    LESSGREATER,
+	golexer.GREATER_THAN: LESSGREATER,
+	golexer.PLUS:         SUM,
+	golexer.MINUS:        SUM,
+	golexer.MULTIPLY:     PRODUCT,
+	golexer.DIVIDE:       PRODUCT,
 }
 
 func (p *Program) TokenLiteral() string {
@@ -83,6 +101,10 @@ func (i *Identifier) String() string       { return i.Value }
 func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+func (fl *FloatLiteral) expressionNode()      {}
+func (fl *FloatLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FloatLiteral) String() string       { return fl.Token.Literal }
 
 func (ie *InfixExpression) expressionNode()      {}
 func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
@@ -130,8 +152,10 @@ type Parser struct {
 	lexer  *golexer.Lexer
 	errors []string
 
-	curToken  golexer.Token
-	peekToken golexer.Token
+	curToken       golexer.Token
+	peekToken      golexer.Token
+	prefixParseFns map[golexer.TokenType]PrefixParsefn
+	infixParseFns  map[golexer.TokenType]InfixParsefn
 }
 
 func (p *Parser) nextToken() {
@@ -157,6 +181,23 @@ func (p *Parser) peekError(t golexer.TokenType) {
 	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
 	p.errors = append(p.errors, msg)
 }
+func (p *Parser) noPrefixParseFnError(t golexer.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+func (p *Parser) peekPrecedence() int {
+	if prec, ok := precedences[p.peekToken.Type]; ok {
+		return prec
+	}
+	return LOWEST
+}
+func (p *Parser) currentPrecedence() int {
+	if prec, ok := precedences[p.curToken.Type]; ok {
+		return prec
+	}
+	return LOWEST
+}
+
 func (p *Parser) Errors() []string {
 	return p.errors
 }
@@ -166,9 +207,30 @@ func NewParser(lexer *golexer.Lexer) *Parser {
 		lexer:  lexer,
 		errors: []string{},
 	}
+	p.prefixParseFns = make(map[golexer.TokenType]PrefixParsefn)
+	p.infixParseFns = make(map[golexer.TokenType]InfixParsefn)
+	p.registerPrefix(golexer.IDENT, p.parseIdentifier)
+	p.registerPrefix(golexer.NUMBER, p.parseIntegerLiteral)
+	p.registerPrefix(golexer.LPAREN, p.parseGroupedExpression)
+
+	// infix expressions
+	p.registerInfix(golexer.PLUS, p.parseInfixExpression)
+	p.registerInfix(golexer.MINUS, p.parseInfixExpression)
+	p.registerInfix(golexer.MULTIPLY, p.parseInfixExpression)
+	p.registerInfix(golexer.DIVIDE, p.parseInfixExpression)
+	p.registerInfix(golexer.EQL, p.parseInfixExpression)
+	p.registerInfix(golexer.NOT_EQL, p.parseInfixExpression)
+	p.registerInfix(golexer.LESS_THAN, p.parseInfixExpression)
+	p.registerInfix(golexer.GREATER_THAN, p.parseInfixExpression)
 	p.nextToken()
 	p.nextToken()
 	return p
+}
+func (p *Parser) registerPrefix(tokenType golexer.TokenType, fn PrefixParsefn) {
+	p.prefixParseFns[tokenType] = fn
+}
+func (p *Parser) registerInfix(tokenType golexer.TokenType, fn InfixParsefn) {
+	p.infixParseFns[tokenType] = fn
 }
 func (p *Parser) Parse() *Program {
 	program := &Program{}
@@ -188,8 +250,113 @@ func (p *Parser) parseStatment() Statement {
 		return p.parseLteStatment()
 	case golexer.RETURN:
 		return p.parseReturnStatment()
+	case golexer.IDENT:
+		return p.parseExpressionStatment()
+	case golexer.NUMBER:
+		return p.parseExpressionStatment()
 	default:
 		return p.parseExpressionStatment()
 	}
 
+}
+func (p *Parser) parseLteStatment() *LetStatement {
+	stmt := &LetStatement{Token: p.curToken}
+	if !p.expectPeek(golexer.IDENT) {
+		return nil
+	}
+	stmt.Name = &Identifier{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
+	if !p.expectPeek(golexer.ASSIGN) {
+		return nil
+	}
+	p.nextToken()
+	stmt.Value = p.parseExpression(LOWEST)
+	if !p.peekTokenIs(golexer.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+func (p *Parser) parseReturnStatment() *ReturnStatement {
+	stmt := &ReturnStatement{Token: p.curToken}
+	p.nextToken()
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+	if !p.peekTokenIs(golexer.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+func (p *Parser) parseExpressionStatment() *ExpressionStatement {
+	stmt := &ExpressionStatement{Token: p.curToken}
+	p.nextToken()
+	stmt.Expression = p.parseExpression(LOWEST)
+	if !p.peekTokenIs(golexer.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+func (p *Parser) parseExpression(precedence int) Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
+		return nil
+	}
+	leftExp := prefix()
+	for !p.peekTokenIs(golexer.SEMICOLON) && precedence < p.peekPrecedence() {
+		infix := p.infixParseFns[p.peekToken.Type]
+		if infix == nil {
+			return leftExp
+		}
+		p.nextToken()
+		leftExp = infix(leftExp)
+	}
+	return leftExp
+}
+func (p *Parser) parseIdentifier() Expression {
+	return &Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+func (p *Parser) parseIntegerLiteral() Expression {
+	// Check if the literal contains a decimal point (float)
+	if strings.Contains(p.curToken.Literal, ".") {
+		lit := &FloatLiteral{Token: p.curToken}
+		value, err := strconv.ParseFloat(p.curToken.Literal, 64)
+		if err != nil {
+			msg := fmt.Sprintf("could not parse %q as float", p.curToken.Literal)
+			p.errors = append(p.errors, msg)
+			return nil
+		}
+		lit.Value = value
+		return lit
+	}
+
+	lit := &IntegerLiteral{Token: p.curToken}
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+	return lit
+}
+func (p *Parser) parseInfixExpression(left Expression) Expression {
+	expression := &InfixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+		Left:     left,
+	}
+	precedence := p.currentPrecedence()
+	p.nextToken()
+	expression.Right = p.parseExpression(precedence)
+	return expression
+
+}
+func (p *Parser) parseGroupedExpression() Expression {
+	p.nextToken()
+	exp := p.parseExpression(LOWEST)
+	if !p.expectPeek(golexer.RPAREN) {
+		return nil
+	}
+	return exp
 }
